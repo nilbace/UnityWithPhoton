3e50ae0088aed465899f83f0c8822c6832902e10
nilbace <nilbace@naver.com>
2022-10-27 23:29:49 +0900

    동기화 구현
    간단한 동기화 구현

--- PhotonExample/Assets/Scripts/PlayerMove.cs
+++ PhotonExample/Assets/Scripts/PlayerMove.cs
@@ -8,15 +8,16 @@ using UnityEngine.UI;
 public class PlayerMove : MonoBehaviourPunCallbacks
 {
     public float moveSpeed = 2f;
-    void Start()
-    {
-        
-    }
+    
+    public PhotonView PV;
 
     void Update()
     {
-        float Haxis = Input.GetAxis("Horizontal")*Time.deltaTime*moveSpeed;
-        float Vaxis = Input.GetAxis("Vertical")*Time.deltaTime*moveSpeed;
-        transform.Translate(new Vector3(Haxis, Vaxis, 0));
+        if(PV.IsMine)
+        {
+            float Haxis = Input.GetAxis("Horizontal")*Time.deltaTime*moveSpeed;
+            float Vaxis = Input.GetAxis("Vertical")*Time.deltaTime*moveSpeed;
+            transform.Translate(new Vector3(Haxis, Vaxis, 0));
+        }
     }
 }
